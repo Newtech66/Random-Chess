3b74515b346ccd4e55bc7a8538e76452a40771c3
Newtech66 <78967475+Newtech66@users.noreply.github.com>
2025-04-04 10:56:48 +0530

    add cpu player

--- game.py
+++ game.py
@@ -1,32 +1,42 @@
-import chess
-import random
-import os
+import chess, chess.engine
+import chess.svg
+import cairosvg
+import random, os
+
+# Set this to wherever your chess engine is located
+CHESS_ENGINE_PATH = 'C:\Program Files\stockfish\stockfish-windows-x86-64-avx2.exe'
 
 class Game:
     def __init__(self, white_player, black_player):
         self.board = chess.Board()
-        self.players = [white_player, black_player]
-        self.turn = 0 # 0 is white, 1 is black
-    
+        self.players = [black_player, white_player]
+
     def show_board(self):
         print(self.board)
-    
+        print(f'{self.players[self.board.turn].name} is playing {chess.COLOR_NAMES[self.board.turn]}!')
+
     def is_game_over(self):
         return self.board.is_game_over()
-    
-    def outcome(self):
-        return self.board.outcome()
+
+    def end_game(self):
+        outcome = self.board.outcome()
+        if not outcome.winner:
+            print(f'The game has been drawn.')
+        else:
+            print(f'{white_player.name if outcome.winner else black_player.name} wins by {outcome.termination.name}!')
+        for player in self.players:
+            if isinstance(player, CPUPlayer):
+                player.quit()
     
     def play_move(self, move):
         self.board.push(move)
-        self.turn ^= 1
-    
+
     def current_player(self):
-        return self.players[self.turn]
-    
+        return self.players[self.board.turn]
+
     def _move_count(self):
         return (self.board.legal_moves.count() + 4) // 5
-    
+
     def get_moves(self):
         available_moves = list(self.board.legal_moves)
         k = self._move_count()
@@ -35,29 +45,47 @@ class Game:
 class HumanPlayer:
     def __init__(self, name):
         self.name = name
-    
-    def request_move(self, move_list: list[chess.Move]):
+
+    def request_move(self, move_list: list[chess.Move], board):
         print(self.name + ', please input your move:')
         for i, move in enumerate(move_list, 1):
             print(f'{i}: {move}')
-        print('Please input the move number: ', end='')
-        n = int(input())
-        return move_list[n - 1]
-            
-    
-white_player = HumanPlayer('White')
-black_player = HumanPlayer('Black')
+        while True:
+            print('Please input the move number: ', end='')
+            s = input()
+            try:
+                n = int(s)
+                if 1 <= n <= len(move_list):
+                    return move_list[n - 1]
+                else:
+                    raise ValueError
+            except ValueError:
+                print(f'Please enter an integer between 1 to {len(move_list)}.')
+
+class CPUPlayer:
+    def __init__(self, name):
+        self.name = name
+        self.engine = chess.engine.SimpleEngine.popen_uci(CHESS_ENGINE_PATH)
+
+    def request_move(self, move_list: list[chess.Move], board):
+        result = self.engine.play(board, chess.engine.Limit(time=0.1), root_moves=move_list)
+        return result.move
+
+    def quit(self):
+        self.engine.quit()
+
+white_player = CPUPlayer('White')
+black_player = CPUPlayer('Black')
 game = Game(white_player, black_player)
 
 while not game.is_game_over():
     os.system('cls')
     game.show_board()
     moves = game.get_moves()
-    played_move = game.current_player().request_move(moves)
+    played_move = game.current_player().request_move(moves, game.board)
     game.play_move(played_move)
 
-outcome = game.outcome()
-if not outcome.winner:
-    print(f'The game has been drawn.')
-else:
-    print(f'{white_player.name if outcome.winner else black_player.name} wins by {outcome.termination.name}!')
+game.end_game()
+print(game.board)
+svg_board = chess.svg.board(board=game.board, size=600)
+cairosvg.svg2png(bytestring=svg_board.encode(), write_to='./board.png', output_width=720, output_height=720)
