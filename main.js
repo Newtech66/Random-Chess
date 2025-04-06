a28cca3da52e6108d4502d0ea56bb9fbcc52cd3c
Newtech66 <78967475+Newtech66@users.noreply.github.com>
2025-04-06 23:20:37 +0530

    websocket server check

--- main.js
+++ main.js
@@ -21,12 +21,22 @@ function prepare(websocket) {
   });
 }
 
+function getWebSocketServer() {
+  if (window.location.host === "python-websockets.github.io") {
+    return "wss://principal-heddie-clickboom-05c782d9.koyeb.app/";
+  } else if (window.location.host === "localhost:8000") {
+    return "ws://localhost:8001/";
+  } else {
+    throw new Error(`Unsupported host: ${window.location.host}`);
+  }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   // Initialize the UI.
   const frame = document.querySelector(".frame");
   // Open the WebSocket connection and register event handlers.
   console.log('Opening connection...')
-  websocket = new WebSocket("ws://localhost:8001/");
+  websocket = new WebSocket(getWebSocketServer());
   websocket.addEventListener('close', () => console.log('Closing connection...'));
   prepare(websocket);
   loadContent('home');
